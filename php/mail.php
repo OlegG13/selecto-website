da519347a9e091119fb3a9f86f428cc7c980a16c
k_sergiy <s.kosenchuk@elantixtech.com>
2017-06-16 17:30:27 +0300

    add headers to terms and privacy pages

--- php/mail.php
+++ php/mail.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting (E_ALL);
+ini_set('display_errors', "on");
     function cleanText($var){
         return trim(strip_tags($var));
     }
@@ -29,6 +30,47 @@
         if (!empty($req['message'])) $message = $req['message'];
         if (!empty($req['budget'])) $budget = $req['budget'];
         if (!empty($req['company'])) $company = $req['company'];
+
+				$arr = array(
+					'properties' => array(
+						array(
+							'property' => 'email',
+							'value' => $email
+						),
+						array(
+							'property' => 'firstname',
+							'value' => $name
+						),
+						array(
+							'property' => 'message',
+							'value' => $message . " BUDGET:" . $budget
+						),
+						array(
+							'property' => 'company',
+							'value' => $company
+						)
+					)
+				);
+				$post_json = json_encode($arr);
+				$hapikey = readline("3789f672-6007-4777-b689-88cab8e2c953");
+				$endpoint = 'https://api.hubapi.com/contacts/v1/contact?hapikey=3789f672-6007-4777-b689-88cab8e2c953';
+				$ch = @curl_init();
+				@curl_setopt($ch, CURLOPT_POST, true);
+				@curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
+				@curl_setopt($ch, CURLOPT_URL, $endpoint);
+				@curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+				@curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				$response = @curl_exec($ch);
+				$status_code = @curl_getinfo($ch, CURLINFO_HTTP_CODE);
+				$curl_errors = curl_error($ch);
+				@curl_close($ch);
+				//echo "curl Errors: " . $curl_errors;
+				//echo "\nStatus code: " . $status_code;
+				//echo "\nResponse: " . $response;
+			//-------------------
+			
+			
+			
 
         $mes = "Имя: " .$name . "\nПочта: ".$email . "\nСообщение: ".$message."\n Бюджет ". $budget. "\n Компания: " . $company;
         $subject = 'Site message from - ' . $name;
